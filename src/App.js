6dbf89697a918c032d3afae552d9c05fbd336e13
Md. Solayman <mdsolaymannuhin@gmail.com>
2021-03-19 12:32:07 +0600

    BasicStructureDone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Destination from "./components/Destination/Destination";
+import Blog from "./components/Blog/Blog";
+import Contact from "./components/Contact/Contact";
+import Login from "./components/Login/Login";
+import NoMatch from "./components/NoMatch/NoMatch";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/destination">
+            <Destination></Destination>
+          </Route>
+          <Route path="/blog">
+            <Blog></Blog>
+          </Route>
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NoMatch></NoMatch>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Destination/Destination.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Destination = () => {
+  return (
+    <div>
+      <h1>I am from Destination</h1>
+    </div>
+  );
+};
+
+export default Destination;
